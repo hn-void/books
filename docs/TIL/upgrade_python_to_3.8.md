91782f4aabde853442a8692bed575db419b9737d
Kazuki Nguyen <kazukiyoshida96@gmail.com>
2021-06-05 14:17:44 -0700

    :fire: REMOVE unused files

--- docs/TIL/upgrade_python_to_3.8.md
+++
@@ -1,4 +0,0 @@
-```zsh
-$ brew install python@3.8
-cp /usr/local/Cellar/python@3.8/3.8.2/bin/* /usr/local/bin/
-```
--- docs/gen_html.sh
+++ docs/gen_html.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 pandoc index.md -o index.html
-pandoc til.md -o til.html
-pandoc movies.md -o movies.html
 python3 add_viewport.py
